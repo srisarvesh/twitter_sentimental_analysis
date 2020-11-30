5996b40ab1833efa3ba44889d5e9d98683d43c72
Sarvesh <r.srisarvesh@gmail.com>
2020-12-01 01:17:25 +0530

    After creating the first linear regression model

--- LinearRegressionModel.py
+++ LinearRegressionModel.py
@@ -1,5 +1,9 @@
 import tweepy
+import numpy as np
+from sklearn import linear_model
+import matplotlib.pyplot as plt
 from SimpleTwitterBot import ACCESS_TOKEN, ACCESS_TOKEN_SECRET, API_KEY, API_SECRET_KEY
+
 
 
 def get_twitter_api(API_SECRET_KEY=None, API_KEY=None, ACCESS_TOKEN_SECRET=None, ACCESS_TOKEN=None):
@@ -28,6 +32,23 @@ def main():
     favorite_data,retweet_data=collect_twitter_data(api,"@actorvijay")
     print(favorite_data)
     print(retweet_data)
+    #reshape row data to column data
+    retweet_data_col=np.array(retweet_data).reshape((-1,1))
+    print(retweet_data_col)
+
+    regr=linear_model.LinearRegression()
+    regr.fit(retweet_data_col,favorite_data)
+    print("Coeffienients",regr.coef_)
+    print("Intercepts",regr.intercept_)
+
+    x=np.array(range(0,max(retweet_data)))
+    y=eval('regr.coef_*x+regr.intercept_')
+    plt.plot(x,y)
+    plt.scatter(retweet_data,favorite_data,color='red')
+    plt.xlabel("Retweets")
+    plt.ylabel("Favorites")
+    plt.show()
+
 
 if __name__=="__main__":
     main()
